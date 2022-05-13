9af51554f70500974d24518fd6a70bed00ea1856
Thuynt2003 <101488871+Thuynt2003@users.noreply.github.com>
2022-05-13 11:58:33 +0700

    baithi

---
+++ baithi.js
@@ -0,0 +1,17 @@
+function nextSlide  ()  {
+    let allSlides = document.querySelectorAll(".slideImg");
+    for(let i = 0; i < allSlides.length; i++) {
+        if(allSlides[i].className.includes("activeSlide")) {
+            if(i+1 == allSlides.length) {
+                allSlides[0].className = "slideImg activeSlide";
+                allSlides[i].className = "slideImg hiddenSlide";
+                break;
+            } else {
+                allSlides[i+1].className = "slideImg activeSlide";
+                allSlides[i].className = "slideImg hiddenSlide";
+                break;
+            }
+        }
+    }
+}
+setInterval(() => nextSlide(), 3000);
